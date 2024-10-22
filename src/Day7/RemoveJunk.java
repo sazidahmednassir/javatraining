605d22c06758fb72783182dea507bbfe9a249415
sazidahmednassir <sazidahmednassir@gmail.com>
2024-10-22 10:43:07 +0600

    remove junk character

---
+++ src/Day7/RemoveJunk.java
@@ -0,0 +1,13 @@
+package Day7;
+
+public class RemoveJunk {
+
+    public static void main(String[] args){
+
+        String s="%danmks$324";
+
+        s=s.replaceAll("[^a-zA-Z0-9]", "");
+
+        System.out.println(s);
+    }
+}
